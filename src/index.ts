c5c05f6028ccd1b403d823d8720256687571af1a
Sumna Islam <sumanislam1416@gmail.com>
2022-01-08 23:24:46 +0600

    implemnet google map

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,13 @@ import { Company } from "./Company";
 
 const user = new User();
 const company = new Company();
-
 console.log(user, company);
+
+const mapDiv = document.getElementById('map');
+new google.maps.Map(mapDiv, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
